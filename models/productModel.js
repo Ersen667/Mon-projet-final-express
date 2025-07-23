8806dfabe2fa99b444a7d175826e713168232dac
Ersen667 <marcersenlubin98@gmail.com>
2025-07-23 14:17:11 -0400

    Update productModel.js
    Lubin Marc Ersen

--- models/productModel.js
+++ models/productModel.js
@@ -1,9 +1,32 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  description: String,
+  name: {
+    type: String,
+    required: [true, 'Le prix du produit'],
+    trim:true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Le prix du produit'],
+    min: [0,'Le prix ne peuvent pas inferieur a 0'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  category: {
+    type: String,
+    default:'Divers',
+  },
+  instock: {
+    type: Boolean,
+    default: true,
+  },
+  createAt: {
+    type: Date,
+    default: Date.NOW
+  },
   
 });
 
