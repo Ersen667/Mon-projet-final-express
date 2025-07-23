43c94795227adbeae7a91fa5558640c24652a413
Ersen667 <marcersenlubin98@gmail.com>
2025-07-23 12:33:19 -0400

    Update productModel.js

--- models/productModel.js
+++ models/productModel.js
@@ -1,9 +1,33 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  description: String,
+  name:{
+    type: String,
+    required: [true, 'Le nom du produit'],
+    trim: true,
+  },
+  price:{
+    number,
+    required: [true, 'Le prix du produit'],
+    min: [0, 'Le prix ne peut pas etre plus petit que 0'],
+  },
+  description:{
+    type: String,
+    trim: true,
+    default: 'Aucune description',
+  },
+  category:{
+    type: String,
+    default: 'Divers',
+  },
+  instock:{
+    type: Boolean,
+    default: true,
+  },
+  createAt:{
+    type: Date,
+    default: Date.now,
+  },
   
 });
 
